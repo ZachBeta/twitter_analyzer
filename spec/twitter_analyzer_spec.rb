42ba134dc97005e7943aca5c24d13e22e4f22b24
Zach Morek <zmorek@gmail.com>
2012-06-05 17:46:11 -0400

    set up a before block to DRY out code

--- spec/twitter_analyzer_spec.rb
+++ spec/twitter_analyzer_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 module TwitterAnalyzerSpec
   describe "When called" do
-    it "should return an empty word list after init" do
-      WordCounter.new.word_list.should be_true
+    before(:each) do
+      @wc = WordCounter.new
     end
+    it "should return an empty word list after init" do
+      @wc.word_list.should be_true
+    end
+    it "should add new words" do
+    end
+      
   end
 end
